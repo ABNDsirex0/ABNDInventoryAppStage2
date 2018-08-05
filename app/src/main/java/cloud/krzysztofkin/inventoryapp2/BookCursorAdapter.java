bee1772fcfe5135b395794fa59873d7252a438a4
Krzysztof Kin <sirex0@gmail.com>
2018-08-05 12:17:36 +0200

    listView sell button listener

--- app/src/main/java/cloud/krzysztofkin/inventoryapp2/BookCursorAdapter.java
+++ app/src/main/java/cloud/krzysztofkin/inventoryapp2/BookCursorAdapter.java
@@ -1,11 +1,19 @@
 package cloud.krzysztofkin.inventoryapp2;
 
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import cloud.krzysztofkin.inventoryapp2.data.BookContract.BookEntry;
 
 public class BookCursorAdapter extends CursorAdapter {
     /**
@@ -24,7 +32,41 @@ public class BookCursorAdapter extends CursorAdapter {
 
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, final Context context, Cursor cursor) {
 
+        TextView nameTextView = view.findViewById(R.id.item_name);
+        TextView priceTextView = view.findViewById(R.id.item_price);
+        final TextView quantityTextView = view.findViewById(R.id.item_quantity);
+        Button sellButton = view.findViewById(R.id.item_sell);
+
+        int idColumnIndex = cursor.getColumnIndex(BookEntry._ID);
+        int nameColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_NAME);
+        int quantityColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_QUANTITY);
+        int priceColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_PRICE);
+
+        final int bookID = cursor.getInt(idColumnIndex);
+        String bookName = cursor.getString(nameColumnIndex);
+        final int bookQuantity = cursor.getInt(quantityColumnIndex);
+        int bookPrice = cursor.getInt(priceColumnIndex);
+
+        nameTextView.setText(bookName);
+        priceTextView.setText(Integer.toString(bookPrice));
+        quantityTextView.setText(Integer.toString(bookQuantity));
+
+        sellButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if(bookQuantity>0){
+                    ContentValues values = new ContentValues();
+                    values.put(BookEntry.COLUMN_BOOK_QUANTITY,bookQuantity-1);
+
+                    Uri currentPetUri = ContentUris.withAppendedId(BookEntry.CONTENT_URI, bookID);
+                    context.getContentResolver().update(currentPetUri, values, null, null);
+                }
+                //TODO after list listener implementation check sell button and disable log
+                Log.i("Lista","click sell");
+            }
+        });
     }
 }
