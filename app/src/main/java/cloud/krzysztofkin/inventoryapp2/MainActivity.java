462831f3861c66d5cb66a0acf8ed68bef7b1bb21
Krzysztof Kin <sirex0@gmail.com>
2018-08-05 09:26:36 +0200

    loader

--- app/src/main/java/cloud/krzysztofkin/inventoryapp2/MainActivity.java
+++ app/src/main/java/cloud/krzysztofkin/inventoryapp2/MainActivity.java
@@ -1,6 +1,10 @@
 package cloud.krzysztofkin.inventoryapp2;
 
+import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,27 +12,31 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import cloud.krzysztofkin.inventoryapp2.data.BookContract.BookEntry;
-import cloud.krzysztofkin.inventoryapp2.data.BookDbHelper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
     /**
      * Adapter for listView
      **/
     BookCursorAdapter cursorAdapter;
+
+    /** Identifier for the book data loader */
+    private static final int BOOK_LOADER = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //TODO obsługa listview z adapterem - loader
-        ListView bookListView = (ListView) findViewById(R.id.list);
+        ListView bookListView = findViewById(R.id.list);
         View emptyView = findViewById(R.id.empty_list_view);
         bookListView.setEmptyView(emptyView);
-        cursorAdapter = new BookCursorAdapter(this,null);
+        cursorAdapter = new BookCursorAdapter(this, null);
         bookListView.setAdapter(cursorAdapter);
+        // Kick off the loader
+        getLoaderManager().initLoader(BOOK_LOADER, null, this);
     }
 
     @Override
@@ -70,6 +78,33 @@ public class MainActivity extends AppCompatActivity {
         values.put(BookEntry.COLUMN_BOOK_PHONE, "77777");
         //use content resolver to add data
         Uri newUri = getContentResolver().insert(BookEntry.CONTENT_URI, values);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        // Define a projection that specifies the columns from the table we care about.
+        String[] projection = {
+                BookEntry._ID,
+                BookEntry.COLUMN_BOOK_NAME,
+                BookEntry.COLUMN_BOOK_PRICE,
+                BookEntry.COLUMN_BOOK_QUANTITY};
+        // This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,
+                BookEntry.CONTENT_URI,
+                projection,
+                null,
+                null,
+                null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        cursorAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        cursorAdapter.swapCursor(null);
     }
 }
 
